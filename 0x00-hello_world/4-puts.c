7b7f4869060af8810bfe422d47360cc8031e3e90
Bennygitau <bensonkamaugitau@gmail.com>
2023-02-17 10:21:58 -0800

    a program that use function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Description: prints a string using function puts
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
