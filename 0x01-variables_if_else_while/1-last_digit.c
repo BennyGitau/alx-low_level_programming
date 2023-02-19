debfd5717f9242dab19bbe753422f436519db476
Bennygitau <bensonkamaugitau@gmail.com>
2023-02-19 04:50:35 -0800

    lower case letters

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if (b == 0)
 	{
-		printf("last digit of %d is %d and is 0\n", n, b);
+		printf("Last digit of %d is %d and is 0\n", n, b);
 	}
 	else
 	{
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lover case
+ * Return: (0) success
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
+
