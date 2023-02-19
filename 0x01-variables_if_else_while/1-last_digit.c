cca71f49860960b7a943b9283100c3802e25a00f
Bennygitau <bensonkamaugitau@gmail.com>
2023-02-19 04:30:00 -0800

    compares the last digit of n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - assign and print the last digit of n
+ * Return: (0) success
+ */
+int main(void)
+{
+	int n;
+	int b;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	b = n % 10;
+	if (b > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, b);
+	}
+	else if (b == 0)
+	{
+		printf("last digit of %d is %d and is 0\n", n, b);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, b);
+	}
+	return (0);
+}
