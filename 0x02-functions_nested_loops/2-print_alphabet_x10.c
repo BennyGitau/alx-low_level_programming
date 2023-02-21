183679abe23053410a18fc51bf11de6130e8977b
Bennygitau <bensonkamaugitau@gmail.com>
2023-02-21 15:10:48 -0800

    10 line of lowercase letters

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lowercase letter ten times
+ * Return: success (0)
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+	int count;
+
+	count = 0;
+
+	while (count <= 10)
+	{
+		i = 'a';
+		while (i <= 'z')
+		{
+			putchar(i);
+			count++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
