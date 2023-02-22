babe7f697f48b251d320ce427f47965ac8c834b5
Bennygitau <bensonkamaugitau@gmail.com>
2023-02-22 07:26:43 -0800

    Prints lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - compares lowercase or uppercase
+ * @c: prints the character
+ * Return: always 0
+ */
+int _isalpha(int c)
+{
+	return ((c = 'a' && c <= 'z') || (c = 'A' && c <= 'Z'));
+}
