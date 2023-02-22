3a135ca56d5ad75f6dfaf4182a34a77a4899d521
Bennygitau <bensonkamaugitau@gmail.com>
2023-02-22 10:48:04 -0800

    prints to 98

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * times_table prints nine times table starting with 0
- * a = row, b = column, c = digit
+ * @a: = row, @b: = column, @c: = digit
  * Return: table 0
  */
 void times_table(void)
