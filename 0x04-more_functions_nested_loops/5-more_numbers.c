bd79ee3b13de49d709289e3a5e019e956f344433
Bennygitau <bensonkamaugitau@gmail.com>
2023-02-27 10:47:22 -0800

    prints a line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,16 @@ void more_numbers(void)
 {
 	int a, n;
 
-	for (n = 1; n <= 10; n++)
+	for (n = 1; n < 10; n++)
 	{
-		for (a = 0; a <= 14; a++)
-		{
-			_putchar(a + '0');
-		}
-		_putchar('\n');
+	for (a = 0; a <= 14; a++)
+	{
+	if (a > 9)
+	{
+	_putchar((a / 10) + '0');
+	}
+	_putchar((a % 10) + '0');
+	}
+	_putchar('\n');
 	}
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws a straignt line
+ * @n: number of line
+ * Return: lines
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
