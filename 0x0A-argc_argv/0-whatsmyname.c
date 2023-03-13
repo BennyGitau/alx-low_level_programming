03b438503fa4477c51b126746194fc712e4cc6f9
Bennygitau <bensonkamaugitau@gmail.com>
2023-03-13 11:58:57 -0700

    Prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: argumet vector
+ *Return: exits success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n",*argv);
+	return (0);
+}
