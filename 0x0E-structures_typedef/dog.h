7dc6b4fa74d866df2c733eaa79b07eb1a0858d67
Bennygitau <bensonkamaugitau@gmail.com>
2023-03-22 20:06:55 -0700

    structure declaration

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - structure declaration
+ * @name: name of dog
+ * @age: age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
