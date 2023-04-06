297780f21e4763753a7b35e511992c99751319cc
BennyGitau <bensonkamaugitau@gmail.com>
2023-04-05 19:30:33 -0700

    adds a node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!*head || !new)
 		return (NULL);
 
-	/*assign value to new*/
+
 	new->n = n;
 	new->next = NULL;
 
@@ -32,6 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
+
 	for (i = 0; transv && i < idx; i++)
 	{
 	if (i == idx - 1)
